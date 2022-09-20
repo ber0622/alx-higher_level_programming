9ffdb67b2437030c4a122e07baa165a99b0e5130
ber0622 <bernmuturi@gmail.com>
2022-09-20 23:27:19 +0300

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:d} = 0x{:01x}".format(i, i))
+for a in range(ord('a'), ord('z') + 1):
+    if chr(a) != 'e' and chr(a) != 'q':
+        print("{:c}".format(a), end='')
